88b44a747abc6c65d356c95be8b8723fcbfbf7b6
xuzefan <xuzefan@banggood.com>
2019-09-23 11:19:38 +0800

    Add：添加v2.0初始代码

---
+++ src/main/java/com/xiaofan0408/myprojectnew/MyprojectnewApplication.java
@@ -0,0 +1,13 @@
+package com.xiaofan0408.myprojectnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyprojectnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyprojectnewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xiaofan0408/myprojectnew/MyprojectnewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaofan0408.myprojectnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectnewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
